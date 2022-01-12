2a29f3c68901a8208b7fdac20ec7ede4a7975540
Max Barr <mxbarr@gmail.com>
2022-01-12 11:17:21 -0800

    First commit

---
+++ Assignments/predictDestructuring/predictDestructuring.js
@@ -0,0 +1,74 @@
+// Problem 1
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+// randomCar should be Tesla 
+// otherRandomCar should be Mercedes 
+
+// Problem 2
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+// Not sure why name auto changed to strikethrough in VS code but I think name is undefined
+// otherName should be Elon
+
+// Problem 3
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+// password and hashed password should both be 12345 (I got this wrong. hashedPassword was never defined in the object.)
+
+// Problem 4
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+// first == second is false
+// first == third is true
+
+// Problem 5
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+// key should be value
+// secondKey should be [1, 5, 1, 8, 3, 3]
+// secondKey[0] should be 1
+// willThisWork should be 5
