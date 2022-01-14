f7d3546d0c6d17a519ecacc60f014d52d0ed9c61
Max Barr <mxbarr@gmail.com>
2022-01-14 14:33:51 -0800

    First commit

---
+++ React/my-first-react-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Get better at writing JSX</li>
+        <li>Walk the dog</li>
+        <li>Make dinner</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
