8c5743e805ba1f8b21a04a1ff2f01bbc231999cd
Max Barr <mxbarr@gmail.com>
2022-11-30 16:25:05 -0800

    Updated file

--- MERN practice/Fundamentals/predictDestructuring.js
+++ MERN practice/Fundamentals/predictDestructuring.js
@@ -1,2 +1,23 @@
-let x = 'test';
-console.log(x);
+// Problem 1
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+// Problem 2
+
+
+
+// Problem 3
+
+
+
+// Problem 4
+
+
+
+// Problem 5
+
