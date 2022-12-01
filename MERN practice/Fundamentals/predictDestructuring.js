8b4572e0fcf5a38bd15b2983a3fcfc56ccfcbf06
Max Barr <mxbarr@gmail.com>
2022-11-30 16:31:50 -0800

    updated file

--- MERN practice/Fundamentals/predictDestructuring.js
+++ MERN practice/Fundamentals/predictDestructuring.js
@@ -5,15 +5,38 @@ const [ randomCar ] = cars
 const [ ,otherRandomCar ] = cars
 //Predict the output
 console.log(randomCar)
+// output: Tesla
 console.log(otherRandomCar)
+// Mercedes
 
 // Problem 2
 
-
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+// error, variable is deprecated
+console.log(otherName);
+// Elon
 
 // Problem 3
 
-
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+// 12345
+console.log(hashedPassword);
+// error: undefined, not part of person object
 
 // Problem 4
 
