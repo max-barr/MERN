62a9a21bd2d8b126bf4924507befcf7c8fa187e8
Max Barr <mxbarr@gmail.com>
2022-12-02 16:21:02 -0800

    Updated file

--- MERN practice/Fundamentals/restSpread.js
+++ MERN practice/Fundamentals/restSpread.js
@@ -7,3 +7,26 @@ console.log(firstAnimal);
 // capture the REST of the data set with ...
 const [animalOne, animalTwo, ...otherAnimals] = animals;
 console.log(otherAnimals);
+
+const person = {
+    firstName: 'Jason',
+    lastName: 'Tatum',
+    email: 'tatum@celtics.com',
+    password: 'deuce0',
+    addresses: [
+        {
+            address: '1 Boston Lane',
+            city: 'Boston',
+            state: 'MA'
+        },
+        {
+            address: '0 Three Point Arc',
+            city: 'Durham',
+            state: 'NC'
+        }
+    ],
+    createdAt: 12122023
+};
+
+const {firstName, lastName, ...attributes} = person;
+console.log(attributes);
