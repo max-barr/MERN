16ec55081d7d6680cdde61931a7b33e6cb296be6
Max Barr <mxbarr@gmail.com>
2022-12-06 16:29:18 -0800

    Added component to App.js

--- MERN practice/React/first-jsx/src/App.js
+++ MERN practice/React/first-jsx/src/App.js
@@ -4,14 +4,15 @@ import Person from './components/Person';
 
 function App() {
   return (
-    <>
+    <div>
       <h1>Hello World!</h1>
       <h2>List of things to do:</h2>
       <ul>-Walk the dog</ul>
       <ul>-Buy groceries</ul>
       <ul>-Wash dishes</ul>
       <ul>-Build React app</ul>
-    </>
+      <Person />
+    </div>
   );
 }
 
