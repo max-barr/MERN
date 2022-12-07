8edc6c522cc00d537abfd47640769d1510ca5150
Max Barr <mxbarr@gmail.com>
2022-12-06 16:27:45 -0800

    Added new component

--- MERN practice/React/first-jsx/src/App.js
+++ MERN practice/React/first-jsx/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Person from './components/Person';
 
 function App() {
   return (
---
+++ MERN practice/React/first-jsx/src/components/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Person = (props) => {
+    return (
+        <div>
+            <h2>I am a person.</h2>
+        </div>
+    )
+}
+
+export default Person;
